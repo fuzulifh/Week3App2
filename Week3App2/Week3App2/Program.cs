bc9baa47dffaf68515a8bd20d7a27b37ff2a6f5c
Fuzuli Hemidzade <fuzulifh@code.edu.az>
2024-01-28 19:54:22 +0400

    Work

---
+++ Week3App2/Week3App2/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text;
+using System.Transactions;
+
+namespace Week3App2
+{
+    internal class Program
+    {
+        private static object console;
+
+        //for (int i = 1; i < 6; i++)
+        //{ 
+        //    for (int j = 0; j < i; j++)
+        //    {
+        //        Console.Write("*"+" ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    int studentsCount = 3;
+        //    string[] studentNames = new string[studentsCount];
+        //    int[] studentPoints = new int[studentsCount];
+
+        //    for (int i = 0; i < studentNames.Length; i++)
+        //    {
+        //        Console.WriteLine("Enter student name:");
+        //        studentNames[i]=Console.ReadLine();
+        //        Console.WriteLine("Enter students point:");
+        //        studentPoints[i]=int.Parse(Console.ReadLine());
+        //    }
+        //    Console.WriteLine("Name and Points of Students!");
+        //    Console.WriteLine("----------------------------");
+
+        //    for (int i = 0; i < studentNames.Length; i++)
+        //    {
+        //        Console.WriteLine(studentNames[i]+" " + studentPoints[i]);
+
+        //    }
+
+
+
+
+        //        static void Main(string[] args)
+        //        {
+        //            Console.WriteLine("Information about Region and its Population");
+        //            int regionCount = 1;
+        //            string[] regionNames = new string[regionCount];
+        //            int[] regionPopulation = new int[regionCount];
+
+        //            for (int i = 0; i < regionNames.Length; i++)
+        //            {
+        //                Console.WriteLine("Enter Region:");
+        //                regionNames[i] = Console.ReadLine();
+        //                Console.WriteLine("Enter Region Population:");
+        //                regionPopulation[i] = int.Parse(Console.ReadLine());
+        //            }
+        //            Console.WriteLine("Name and Population Region:");
+        //            Console.WriteLine("----------------------------");
+
+        //            for (int i = 0; i < regionNames.Length; i++)
+        //            {
+        //                Console.WriteLine("-"+regionNames[i] + " " + regionPopulation[i]);
+        //            }
+        //        }
+        //    }
+        //}
+
+
+
+
+
+
+
+
+        //        static void Main(string[] args)
+        //        {
+        //            Console.WriteLine("Information about Students IELTS Points");
+
+        //            float[,] arr2d = new float[3, 4];
+
+        //            for (int i = 0; i < 3; i++)
+        //            {
+        //                for (int j = 0; j < 4; j++)
+        //                {
+        //                    Console.Write($"{i + 1}-st student {j+1} Enter the point: ");
+        //                    float point = float.Parse(Console.ReadLine());
+        //                    arr2d[i, j]=point;
+        //                }
+        //            }
+        //            Console.WriteLine("Student Points:");
+        //            Console.WriteLine("----------------");
+        //            for (int i = 0; i < 3; i++)
+        //            {
+        //                for (int j = 0; j < 4; j++)
+        //                {
+        //                    //Console.WriteLine($"{i + 1}-st Student Point: {arr2d[i,j]} ");
+        //                    Console.Write(arr2d[i, j] + "|");
+        //                }
+        //                Console.WriteLine();
+        //            }
+        //        }
+        //    }
+        //}
+
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Enter Student Count:");
+        //    int studentCount =int.Parse(Console.ReadLine());
+        //    string[] students=new string[studentCount];
+
+        //    for (int i = 0; i < studentCount; i++)
+        //    {
+        //        Console.WriteLine("Enter Students Name:");
+        //        string student = Console.ReadLine();
+        //        students[i] = student;
+
+        //    }
+        //    Console.WriteLine("The Name of Students:");
+        //    Console.WriteLine("---------------------");
+
+        //    for (int i = 0; i < studentCount; i++)
+        //    {
+        //        Console.WriteLine(students[i] + "");
+        //    }
+
+
+        //Console.WriteLine("---------------------");
+        //foreach (var item in students)
+        //{
+        //    Console.WriteLine(item);
+        //}
+//        static void Main(string[] args)
+//        {
+//            List<int> list = new List<int>();
+            
+//            do
+//            {
+//            Console.WriteLine("Enter Student Count:");
+//            int grade = int.Parse(Console.ReadLine());
+//                grade.Add(grade);
+            
+//            }
+//            while (true);
+            
+           
+//        }
+//    }
+//}
+
+
+
+    
